6651fcda09a648f58df04795d5b39cf3c33c0156
gitluky <jluk87@gmail.com>
2017-09-08 17:38:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,21 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
+function addElementToBeginningOfArray (array, element) {
+  var newArray = array.unshift(element)
+  return newArray
+}
 
-var newChoc = ['butter finger', ...chocolateBars]
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array = array.unshift(element)
+  return array
+}
 
-console.log(newChoc)
+function addElementTOEndofArray(array, element){
+  var newArray = array.push(element)
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array = array.push(element)
+  return array
+}
