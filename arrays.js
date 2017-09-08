a63640ac5a318251cee11ce4a4e57b4cd0e8dd90
gitluky <jluk87@gmail.com>
2017-09-08 17:51:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementTOEndofArray(array, element){
-  var newArray = [...array,element]
+  var newArray = [...array, element]
   return newArray
 }
 
